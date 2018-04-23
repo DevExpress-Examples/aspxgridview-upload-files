8216408c648260129884a4c914026daf9fc2f84c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿// Developer Express Code Central Example:
+// ASPxGridView - How to upload .pdf files in Edit mode and see them on a cell click in Browse mode
+// 
+// This example demonstrates how to implement a custom column with hyperlinks to
+// different files. At first, links are empty. You can upload a file in Edit mode,
+// and this row hyperlink will contain the URL to this file.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E4644
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxUploadControl;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ASPxGridView1_CustomUnboundColumnData(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs e)
+    {
+        if (e.IsGetData)
+        {
+            l = Session["list"] as List<MySavedObjects>;
+            if (l != null)
+            {              
+                    if (l[e.ListSourceRowIndex].FileName != String.Empty && l[e.ListSourceRowIndex].Url != String.Empty)
+                    {
+                        ASPxHyperLink hl = ASPxGridView1.FindRowCellTemplateControl(e.ListSourceRowIndex, (GridViewDataColumn)e.Column, "ASPxHyperLink") as ASPxHyperLink;
+                        hl.Text = l[e.ListSourceRowIndex].FileName;
+                        hl.NavigateUrl = l[e.ListSourceRowIndex].Url;
+                    }
+                           
+            }
+        }
+    }
+    List<MySavedObjects> l;
+    protected void ASPxUploadControl1_FileUploadComplete(object sender, DevExpress.Web.ASPxUploadControl.FileUploadCompleteEventArgs e)
+    {
+        if (e.IsValid)
+        {
+            ASPxUploadControl upc = sender as ASPxUploadControl;
+            if (e.UploadedFile != null)
+            {
+                e.UploadedFile.SaveAs(Server.MapPath("~/Documents/" + e.UploadedFile.FileName), true);
+                if (ASPxGridView1.IsEditing)
+                {
+
+                    l = Session["list"] as List<MySavedObjects>;
+                    if (l != null)
+                    {
+                        l[ASPxGridView1.EditingRowVisibleIndex].Url = Page.ResolveUrl("~/Documents/" + e.UploadedFile.FileName);
+                        l[ASPxGridView1.EditingRowVisibleIndex].FileName = e.UploadedFile.FileName;
+                        Session["list"] = l;
+                    }
+                }
+            }
+        }
+    }
+    protected void ASPxGridView1_DataBound(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            l = new List<MySavedObjects>();
+            for (int i = 0; i < ASPxGridView1.VisibleRowCount; i++)
+            {
+                l.Add(new MySavedObjects() { RowNumber = i});
+            }
+            Session["list"] = l;
+        }
+    }
+    protected void ASPxHyperLink_Load(object sender, EventArgs e)
+    {
+        ASPxHyperLink hpl = sender as ASPxHyperLink;
+        GridViewDataItemTemplateContainer c = hpl.NamingContainer as GridViewDataItemTemplateContainer;
+        l = Session["list"] as List<MySavedObjects>;
+        if (l[c.VisibleIndex].FileName != String.Empty && l[c.VisibleIndex].Url != String.Empty)
+        {
+            hpl.Text = l[c.VisibleIndex].FileName;
+            hpl.NavigateUrl = l[c.VisibleIndex].Url;
+        }
+
+    }
+}
+public class MySavedObjects{
+    public MySavedObjects()
+    {
+        _FileName = String.Empty;
+        _Url = String.Empty;
+    }
+    // Fields...
+    private string _FileName;
+    private string _Url;
+    private int _RowNumber;
+
+    public int RowNumber
+    {
+        get { return _RowNumber; }
+        set { _RowNumber = value; }
+    }
+
+    public string Url
+    {
+        get { return _Url; }
+        set { _Url = value; }
+    }
+
+    public string FileName
+    {
+        get { return _FileName; }
+        set { _FileName = value; }
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,116 @@
+﻿' Developer Express Code Central Example:
+' ASPxGridView - How to upload .pdf files in Edit mode and see them on a cell click in Browse mode
+' 
+' This example demonstrates how to implement a custom column with hyperlinks to
+' different files. At first, links are empty. You can upload a file in Edit mode,
+' and this row hyperlink will contain the URL to this file.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4644
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxUploadControl
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	Protected Sub ASPxGridView1_CustomUnboundColumnData(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs)
+		If e.IsGetData Then
+			l = TryCast(Session("list"), List(Of MySavedObjects))
+			If l IsNot Nothing Then
+					If l(e.ListSourceRowIndex).FileName <> String.Empty AndAlso l(e.ListSourceRowIndex).Url <> String.Empty Then
+						Dim hl As ASPxHyperLink = TryCast(ASPxGridView1.FindRowCellTemplateControl(e.ListSourceRowIndex, CType(e.Column, GridViewDataColumn), "ASPxHyperLink"), ASPxHyperLink)
+						hl.Text = l(e.ListSourceRowIndex).FileName
+						hl.NavigateUrl = l(e.ListSourceRowIndex).Url
+					End If
+
+			End If
+		End If
+	End Sub
+	Private l As List(Of MySavedObjects)
+	Protected Sub ASPxUploadControl1_FileUploadComplete(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxUploadControl.FileUploadCompleteEventArgs)
+		If e.IsValid Then
+			Dim upc As ASPxUploadControl = TryCast(sender, ASPxUploadControl)
+			If e.UploadedFile IsNot Nothing Then
+				e.UploadedFile.SaveAs(Server.MapPath("~/Documents/" & e.UploadedFile.FileName), True)
+				If ASPxGridView1.IsEditing Then
+
+					l = TryCast(Session("list"), List(Of MySavedObjects))
+					If l IsNot Nothing Then
+						l(ASPxGridView1.EditingRowVisibleIndex).Url = Page.ResolveUrl("~/Documents/" & e.UploadedFile.FileName)
+						l(ASPxGridView1.EditingRowVisibleIndex).FileName = e.UploadedFile.FileName
+						Session("list") = l
+					End If
+				End If
+			End If
+		End If
+	End Sub
+	Protected Sub ASPxGridView1_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			l = New List(Of MySavedObjects)()
+			For i As Integer = 0 To ASPxGridView1.VisibleRowCount - 1
+				l.Add(New MySavedObjects() With {.RowNumber = i})
+			Next i
+			Session("list") = l
+		End If
+	End Sub
+	Protected Sub ASPxHyperLink_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim hpl As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+		Dim c As GridViewDataItemTemplateContainer = TryCast(hpl.NamingContainer, GridViewDataItemTemplateContainer)
+		l = TryCast(Session("list"), List(Of MySavedObjects))
+		If l(c.VisibleIndex).FileName <> String.Empty AndAlso l(c.VisibleIndex).Url <> String.Empty Then
+			hpl.Text = l(c.VisibleIndex).FileName
+			hpl.NavigateUrl = l(c.VisibleIndex).Url
+		End If
+
+	End Sub
+End Class
+Public Class MySavedObjects
+	Public Sub New()
+		_FileName = String.Empty
+		_Url = String.Empty
+	End Sub
+	' Fields...
+	Private _FileName As String
+	Private _Url As String
+	Private _RowNumber As Integer
+
+	Public Property RowNumber() As Integer
+		Get
+			Return _RowNumber
+		End Get
+		Set(ByVal value As Integer)
+			_RowNumber = value
+		End Set
+	End Property
+
+	Public Property Url() As String
+		Get
+			Return _Url
+		End Get
+		Set(ByVal value As String)
+			_Url = value
+		End Set
+	End Property
+
+	Public Property FileName() As String
+		Get
+			Return _FileName
+		End Get
+		Set(ByVal value As String)
+			_FileName = value
+		End Set
+	End Property
+End Class
